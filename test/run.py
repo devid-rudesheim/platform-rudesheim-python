627ecb868ac6af0f8434fc027f1d871c0bfa5d29
devid-rudesheim <87573287+devid-rudesheim@users.noreply.github.com>
2024-05-29 20:41:25 +0900

    To restructure test

--- test/run.py
+++ test/run.py
@@ -6,19 +6,20 @@ import unittest as ut
 
 class PlatformTests( ut.TestCase ):
 
-	def test_0( this ):
-		this.assertEqual( "file.name", pf.OS.path()( "/file.name" ).name )
 
-	def test_1( this ):
+	def test_0_0( this ):
 		os = pf.linux.OS
 		this.assertEqual( os, pf.OS.os_by_name( os.name() ) )
 
-	def test_2( this ):
+	def test_0_1( this ):
 		os = pf.mac.OS
 		this.assertEqual( os, pf.OS.os_by_name( os.name() ) )
 
-	def test_3( this ):
+	def test_0_2( this ):
 		os = pf.windows.OS
 		this.assertEqual( os, pf.OS.os_by_name( os.name() ) )
+
+	def test_1( this ):
+		this.assertEqual( "file.name", pf.OS.path()( "/file.name" ).name )
 
 ut.main()
