97b2e709924cc7b2d2292898f756c6a83c9dc57c
devid-rudesheim <87573287+devid-rudesheim@users.noreply.github.com>
2024-05-31 19:57:53 +0900

    Resturecture make dictionary

--- lib/rudesheim/platform/basic.py
+++ lib/rudesheim/platform/basic.py
@@ -7,11 +7,7 @@ class OS:
 
 	@classmethod
 	def os_by_name( this, system_name ):
-		os_dicitionary = {}
-		for i in OS.__subclasses__():
-			os_dicitionary[ i.name() ] = i
-
-		return os_dicitionary.get( system_name, OS )
+		return { i.name() : i for i in OS.__subclasses__() }.get( system_name, OS )
 
 	@classmethod
 	def path( this ):
